7dd61279a81995a1ddf2e15361691f170a56b526
Víctor Noriega <dragon124victor421force@gmail.com>
2021-01-30 20:56:12 -0600

    Random Stuff.

--- App.js
+++ App.js
@@ -1,21 +1,58 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { ScrollView, StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+class App extends React.Component {
+
+  testList() {
+    var items = [];
+    for (var i = 0; i < 100; i++) {
+      if (i % 2 == 0) {
+        items.push(
+          <Text key={i} style={[styles.even, styles.row]}>
+            Nothing to see here!
+          </Text>
+        );
+      } else {
+        items.push(
+          <Text key={i} style={[styles.odd, styles.row]}>
+            Something to see here!
+          </Text>
+        );
+      }
+    }
+    return items;
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <ScrollView>
+          {this.testList()}
+        </ScrollView>
+      </View>
+    );
+    // <StatusBar style="auto" />
+  }
+
 }
+
+export default App;
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    //alignItems: 'center',
+    //justifyContent: 'center',
+  },
+  odd: {
+    backgroundColor: 'white',
+  },
+  even: {
+    backgroundColor: '#D3D3D3',
+  },
+  row: {
+    padding: 3,
   },
 });
